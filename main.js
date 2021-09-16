a100e7291a1a3e18094b275691f21223daeb5f35
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-16 08:15:43 +0800

    Initial commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import ConnectionManager from "./js/ConnectionManager.js"
 
-let connection = new ConnectionManager(`wss://${window.location.host}${window.location.pathname}/ws`);
+let connection = new ConnectionManager(`wss://${window.location.host}${window.location.pathname}ws`);
 connection.connect();
