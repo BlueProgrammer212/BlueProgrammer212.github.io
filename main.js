ab8000a1b5c01999cd8db09fe81ce5871ac27396
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-17 10:04:37 +0800

    Initial commit

--- main.js
+++
@@ -1,8 +0,0 @@
-import ConnectionManager from "./js/ConnectionManager.js"
-import FragmentManager from "./js/FragmentManager.js";
-
-let template = document.getElementById("users");
-let VideoManager = new FragmentManager(template);
-
-VideoManager.add("Gian")
-
