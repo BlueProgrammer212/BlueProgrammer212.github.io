ee093f48044d727451522ed5d81c5e30d1749746
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-25 10:16:32 +0800

    Initial Commit

--- home/comments/main.js
+++ home/comments/main.js
@@ -4,17 +4,28 @@ let checkBox = document.getElementById("checkbox-id-0"),
     proceed = document.getElementById("proceed"),
     message = document.getElementById("invalid");
 checkBox.click = null;
+proceed.disabled = true;
 let disagreed = () => {
     if (!checkBox.firstChild) {
         message.className = message.className.removeClass("invisible");
+        proceed.disabled = true;
     } else {
         message.className += " invisible" 
         if (window.location.hash == "#89679456935") {
             
         }
+        proceed.disabled = false;
         window.location.hash = "#89679456935"  
     }
 };
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    document.getElementsByClassName("pfp_img")[0].src = profile.getImageUrl();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+  }
 
 window.addEventListener("load", () => {
     auth2.attachClickHandler(proceed, {},
