cbe9f3f865e959fee36a3212c58919d34023f0ca
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-25 16:34:51 +0800

    Initial Commit

--- home/comments/main.js
+++ home/comments/main.js
@@ -96,14 +96,7 @@ function loadInformation() {
 
 window.addEventListener("load", () => {
     if (auth2.isSignedIn.get()) {
-      let image_url = getCookie("pfp_url"), 
-          name = getCookie("pf_name"),
-          id = getCookie("pf_id");
-      console.log(`Loading profile... ${new Profile(image_url, id, name)}`)
-      console.log(`Loading username... NAME:${name}`)
-      console.log(`Loading UserID... ID:<${id}>`)
-      console.log(`Loading profile picture ${image_url}...`)  
-      pfp_img_elem.setAttribute("src", image_url);
+     
     }
     auth2.attachClickHandler(proceed, {}, onSignIn, function(error) {
       console.error('An error occured:', JSON.stringify(error, undefined, 2));
@@ -111,6 +104,14 @@ window.addEventListener("load", () => {
       document.getElementById("invalid").className = document.getElementById("invalid").removeClass("invisible");
     });
     console.log(`Loading client content... ${document.body}`)
+    let image_url = getCookie("pfp_url"), 
+    name = getCookie("pf_name"),
+    id = getCookie("pf_id");
+    console.log(`Loading profile... ${new Profile(image_url, id, name)}`)
+    console.log(`Loading username... NAME:${name}`)
+    console.log(`Loading UserID... ID:<${id}>`)
+    console.log(`Loading profile picture ${image_url}...`)  
+    pfp_img_elem.setAttribute("src", image_url);
 })
 
 checkBox.addEventListener("click", () => {
