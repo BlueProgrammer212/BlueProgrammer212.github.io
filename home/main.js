d2613081d4f3373fd1bc66e445bb02080be5841d
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-25 18:24:49 +0800

    Initial Commit

--- home/main.js
+++ home/main.js
@@ -80,5 +80,96 @@ for (let i = 0; i < 3; ++i) {
     fragment.appendTo(document.getElementById("p-id-1"))
 }
 */
+var googleUser = {};
+var startApp = function() {
+  gapi.load('auth2', function(){
+    auth2 = gapi.auth2.init({
+      client_id: '730868686856-lkanp3tois4cj938t2g794cebadtqkoo.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin'
+    });
+  });
+};
 
+startApp()
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile(),
+        id = profile.getId(), 
+        image_url = profile.getImageUrl(),
+        name = profile.getName(),
+        email = profile.getEmail();
+      
+    let pfp_elem = document.getElementsByClassName("pfp_img")[0];
+
+    pfp_elem.setAttribute("src", image_url)
+    
+    console.log('ID: ' + id);
+    console.log('Name: ' + name);
+    console.log('Image URL: ' + image_url);
+    console.log('Email: ' + email);
+
+    setCookie("pfp_url", image_url, 365)
+    setCookie("pf_id", id.toString(), 365)
+    setCookie("pf_name", name, 365)
+    setCookie("pf_email", email, 365);
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+let pfp_img_elem = document.getElementsByClassName("pfp_img")[0];
+
+class Profile {
+  constructor(url, id, name) {
+      this.url = url;
+      this.id = id; 
+      this.name = name;
+  }
+}
+
+function loadInformation() {
+  return new Promise((res) => {
+    if (auth2.isSignedIn.get()) {
+      let image_url = getCookie("pfp_url"), 
+          name = getCookie("pf_name"),
+          id = getCookie("pf_id");
+      console.log(`Successfully loaded client content... ${document.body}`)
+      setTimeout(res, 2000, new Profile(image_url, id, name))
+    }
+  });
+}
+
+window.addEventListener("load", () => {
+    if (!auth2.isSignedIn.get()) {
+      let image_url = getCookie("pfp_url"), 
+      name = getCookie("pf_name"),
+      id = getCookie("pf_id");
+      console.log(`Loading profile... ${new Profile(image_url, id, name)}`)
+      console.log(`Loading username... NAME:${name}`)
+      console.log(`Loading UserID... ID:<${id}>`)
+      console.log(`Loading profile picture ${image_url}...`)  
+      pfp_img_elem.setAttribute("src", image_url);
+      window.location.href = "./page"
+    }
+    console.log(`Loading client content... ${document.body}`)
+})
 console.log("Wanna be a developer?")
