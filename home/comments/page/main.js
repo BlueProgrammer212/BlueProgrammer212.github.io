bd2f80a3b2ce21aaf1358f03ef75fd0715fedc4e
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-26 19:37:58 +0800

    Initial Commit

--- home/comments/page/main.js
+++ home/comments/page/main.js
@@ -36,7 +36,6 @@ function onSignIn(googleUser) {
     setCookie("pf_id", id.toString(), 365)
     setCookie("pf_name", name, 365)
     setCookie("pf_email", email, 365);
-    window.location.href = "https://blueprogrammer212.github.io/home/comments";
 }
 
 function getCookie(cname) {
