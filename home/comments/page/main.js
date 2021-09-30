0aad7f516a639128475b0d6f46a849d2c870f23c
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-09-30 16:08:33 +0800

    Initial Commit

--- home/comments/page/main.js
+++ home/comments/page/main.js
@@ -27,7 +27,6 @@ class CommentManager {
       firestore.collection(`comments`).add(data).catch(err => {
         console.error(new Error(`Server failed to add comment, ${data}. ${err}`))
       })
-      this.parent.appendChild(this.child);
     }
 }
 
