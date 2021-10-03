aa5be3f66911cf29b8c7184aa3b074dcc74d8e1b
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-03 15:08:18 +0800

    Initial Commit

--- home/comments/page/main.js
+++ home/comments/page/main.js
@@ -1,170 +1,174 @@
 (function() {
-
-var googleUser = {};
-let firestore;
-let template_elem = document.getElementById("template_comment"), 
-    parent = document.getElementById("comment_section");
-
-function disableDrag() {
-  for (let i = 0; i < document.images.length; i++) {
-    document.images[i].setAttribute("draggable", false);
+  
+  var googleUser = {};
+  let firestore;
+  let template_elem = document.getElementById("template_comment"), 
+  parent = document.getElementById("comment_section");
+  
+  function disableDrag() {
+    for (let i = 0; i < document.images.length; i++) {
+      document.images[i].setAttribute("draggable", false);
+    }
   }
-}
-
-function clearDatabase() {
-  if (prompt("PIN:")===`#9576`) {
-    firestore.collection('comments').get().then(querySnapshot => {
-      querySnapshot.docs.forEach(snapshot => {
-         // console.log(snapshot)
+  
+  function clearDatabase() {
+    if (prompt("PIN:")===`#9576`) {
+      firestore.collection('comments').get().then(querySnapshot => {
+        querySnapshot.docs.forEach(snapshot => {
+          // console.log(snapshot)
           snapshot.ref.delete();
+        })
       })
+    }
+  }
+  
+  function timeout(time, callback) {
+    return new Promise((res) => {
+      callback();
+      setTimeout(res, time, callback);
     })
   }
-}
-
-function timeout(time, callback) {
-   return new Promise((res) => {
-         callback();
-         setTimeout(res, time, callback);
-   })
-}
-
-class CommentManager {
-   constructor(template, parent) {
+  
+  class CommentManager {
+    constructor(template, parent) {
       this.template = template;
       this.parent = parent;
       this.child = this.template.content.cloneNode(true);
-   }
-
-   generateKey(len=40) {
-       let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-       let string;
-       while (--len > 0) {
-          string += chars[Math.round(Math.random()*chars.length-1)];
-       }
-       return string;
-   }
-
-   async add(comm = []) {
-      let img = document.getElementsByClassName("profile_picture_32x32"),
-          name = document.getElementsByClassName("pfp_name"),
-          comment_msg = document.getElementsByClassName("comment_message");
-      for (let j = 0; j < comm.length; ++j) {
-          await timeout(10, () => {
-              if (!img[j]) {this.child = this.template.content.cloneNode(true);
-              this.parent.appendChild(this.child); }
-          }).then((cb) => {
-             if (name[name.length]) {
-                name[name.length].innerHTML = comm[j].name;
-             } else {
-                name[name.length - 1].innerHTML = comm[j].name;
-             }
-            if (comment_msg[comment_msg.length]) {
-                comment_msg[comment_msg.length].innerHTML = comm[j].content;
-            } else {
-                comment_msg[comment_msg.length - 1].innerHTML = comm[j].content;
-            }
-             if (img[img.length]) {
-                img[img.length].setAttribute("src", comm[j].pfp_link);
-             } else {
-                img[img.length - 1].setAttribute("src", comm[j].pfp_link);
-             }
-          });
-      }
-   }
-   
-   post(data) {
-     if (data.content !== "") {
-        firestore.collection(`comments`).add(data).catch(err => {
-          console.error(new Error(`Server failed to add comment, ${data}. ${err}`))
-        })
-     }
     }
-}
-
-let comments = new CommentManager(template_elem, parent)
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDqcXlXth2r-3nA-nWxUTlcm5-vgq2ZQgA",
-  authDomain: "pixcel-272e8.firebaseapp.com",
-  projectId: "pixcel-272e8",
-  storageBucket: "pixcel-272e8.appspot.com",
-  messagingSenderId: "527485563587",
-  appId: "1:527485563587:web:59c6c095e772a028802876",
-  measurementId: "G-49V48L8TZR",
-  databaseURL: "https://pixcel-272e8-default-rtdb.firebaseio.com"
-};
-
-var startApp = function() {
-  gapi.load('auth2', function(){
-    auth2 = gapi.auth2.init({
-      client_id: '730868686856-lkanp3tois4cj938t2g794cebadtqkoo.apps.googleusercontent.com',
-      cookiepolicy: 'single_host_origin'
-    });
-  });
-};
-
-startApp()
-
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile(),
-        id = profile.getId(), 
-        image_url = profile.getImageUrl(),
-        name = profile.getName(),
-        email = profile.getEmail();
-      
-    let pfp_elem = document.getElementsByClassName("pfp_img")[0];
-
-    pfp_elem.setAttribute("src", image_url)
     
-    console.log('ID: ' + id);
-    console.log('Name: ' + name);
-    console.log('Image URL: ' + image_url);
-    console.log('Email: ' + email);
-
-    setCookie("pfp_url", image_url, 365)
-    setCookie("pf_id", id.toString(), 365)
-    setCookie("pf_name", name, 365)
-    setCookie("pf_email", email, 365);
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    generateKey(len=40) {
+      let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+      let string;
+      while (--len > 0) {
+        string += chars[Math.round(Math.random()*chars.length-1)];
+      }
+      return string;
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+    
+    async add(comm = []) {
+      let img = document.getElementsByClassName("profile_picture_32x32"),
+      name = document.getElementsByClassName("pfp_name"),
+      comment_msg = document.getElementsByClassName("comment_message");
+      for (let j = 0; j < comm.length; ++j) {
+        await timeout(10, () => {
+          if (!img[j]) {this.child = this.template.content.cloneNode(true);
+            this.parent.appendChild(this.child); }
+          }).then((cb) => {
+            if (name[name.length]) {
+              name[name.length].innerHTML = comm[j].name;
+            } else {
+              name[name.length - 1].innerHTML = comm[j].name;
+            }
+            if (comment_msg[comment_msg.length]) {
+              comment_msg[comment_msg.length].innerHTML = comm[j].content;
+            } else {
+              comment_msg[comment_msg.length - 1].innerHTML = comm[j].content;
+            }
+            if (img[img.length]) {
+              img[img.length].setAttribute("src", comm[j].pfp_link);
+            } else {
+              img[img.length - 1].setAttribute("src", comm[j].pfp_link);
+            }
+          });
+        }
+      }
+      
+      post(data) {
+        if (data.content !== "") {
+          firestore.collection(`comments`).add(data).catch(err => {
+            console.error(new Error(`Server failed to add comment, ${data}. ${err}`))
+          })
+        }
+      }
     }
-  }
-  return "";
-}
-
-let pfp_img_elem = document.getElementsByClassName("pfp_img")[0];
-
-class Profile {
-  constructor(url, id, name) {
-      this.url = url;
-      this.id = id; 
-      this.name = name;
-  }
-}
-
-function loadInformation() {
-  return new Promise((res) => {
-    if (auth2.isSignedIn.get()) {
-      let image_url = getCookie("pfp_url"), 
+    
+    let comments = new CommentManager(template_elem, parent)
+    
+    const firebaseConfig = {
+      apiKey: "AIzaSyDqcXlXth2r-3nA-nWxUTlcm5-vgq2ZQgA",
+      authDomain: "pixcel-272e8.firebaseapp.com",
+      projectId: "pixcel-272e8",
+      storageBucket: "pixcel-272e8.appspot.com",
+      messagingSenderId: "527485563587",
+      appId: "1:527485563587:web:59c6c095e772a028802876",
+      measurementId: "G-49V48L8TZR",
+      databaseURL: "https://pixcel-272e8-default-rtdb.firebaseio.com"
+    };
+    
+    var startApp = function() {
+      gapi.load('auth2', function(){
+        auth2 = gapi.auth2.init({
+          client_id: '730868686856-lkanp3tois4cj938t2g794cebadtqkoo.apps.googleusercontent.com',
+          cookiepolicy: 'single_host_origin'
+        });
+      });
+    };
+    
+    startApp()
+    
+    function setCookie(cname, cvalue, exdays) {
+      const d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      let expires = "expires="+ d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    
+    function onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile(),
+      id = profile.getId(), 
+      image_url = profile.getImageUrl(),
+      name = profile.getName(),
+      email = profile.getEmail();
+      
+      let pfp_elem = document.getElementsByClassName("pfp_img")[0];
+      
+      pfp_elem.setAttribute("src", image_url)
+      
+      console.log('ID: ' + id);
+      console.log('Name: ' + name);
+      console.log('Image URL: ' + image_url);
+      console.log('Email: ' + email);
+      
+      setCookie("pfp_url", image_url, 365)
+      setCookie("pf_id", id.toString(), 365)
+      setCookie("pf_name", name, 365)
+      setCookie("pf_email", email, 365);
+    }
+    
+    function getCookie(cname) {
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+    
+    if (location.search == "?b=true" && getCookie("banned")) {
+       location.href = "./ban.html"
+    }
+    
+    let pfp_img_elem = document.getElementsByClassName("pfp_img")[0];
+    
+    class Profile {
+      constructor(url, id, name) {
+        this.url = url;
+        this.id = id; 
+        this.name = name;
+      }
+    }
+    
+    function loadInformation() {
+      return new Promise((res) => {
+        if (auth2.isSignedIn.get()) {
+          let image_url = getCookie("pfp_url"), 
           name = getCookie("pf_name"),
           id = getCookie("pf_id");
       console.log(`Successfully loaded client content... ${document.body}`)
