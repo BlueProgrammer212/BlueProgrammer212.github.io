7e48886fa37896f2cebc99550674c6e25da5a3bf
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-07 13:08:23 +0800

    Initial Commit

--- home/comments/page/main.js
+++ home/comments/page/main.js
@@ -229,6 +229,8 @@ document.getElementById("post_btn").addEventListener("click", () => {
   slug: 'pixcel', content: document.getElementById("post_comment").value, pld: null, 
   pfp_link: getCookie("pfp_url"), name: getCookie("pf_name")});
   document.getElementById("post_comment").value = "";
+  window.localStorage.setItem("prev_text", "");
+  window.location.reload();
 })
 
 document.getElementById("post_comment").addEventListener("input", () => {
