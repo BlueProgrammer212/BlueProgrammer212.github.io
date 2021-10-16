2db0ccc93c432cb818290a4b509ac6e3f164f884
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-16 22:50:44 +0800

    Initial Commit

--- home/community/info.ts
+++ home/community/info.ts
@@ -38,11 +38,8 @@ class FragmentManager {
             inputBox = document.getElementsByClassName("comment_message")[i].innerHTML;
             if (inputBox.length == 0 && !inputBox.startsWith("/uploadImg[")) {
               this.setMessage(message);
-              var matches = inputBox.match(/\[(.*?)\]/);
-              if (matches) {
-                  console.log(matches[1])
-              }
-              document.getElementsByClassName("img_upload")[i].setAttribute("src", matches[1]);
+              document.getElementsByClassName("img_upload")[i].setAttribute("src",
+              inputBox.replace( /(^.*\[|\].*$)/g, '' ););
             }
         }
         this.setImage(pfp_link);
