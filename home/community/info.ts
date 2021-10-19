bf7951202a71d79aad242ae49bd3ed9ada3057d5
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-19 17:58:31 +0800

    Initial Commit

--- home/community/info.ts
+++ home/community/info.ts
@@ -119,7 +119,7 @@ class FragmentManager extends FragmentInstance implements FragmentExtension {
 
         for (let i = 0; i < document.getElementsByClassName("postsBox").length; ++i) {
             this.setPosts(data, i);
-            document.getElementsByClassName("img_button_div")[i].addEventListener("click", () => {
+            document.getElementsByClassName("img_upload")[i].addEventListener("click", () => {
                 window.location.search = `?preview=${data.message.match(/\[(.*?)\]/)[1]}`;
             })
         }
