1132e8b590ce638714acff70b8bb3eb61cb07225
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-24 07:08:13 +0800

    Initial Commit

--- profile/main.ts
+++ profile/main.ts
@@ -1,8 +1,6 @@
 let main_img : any = document.getElementById("img_pfp");
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-main_img.addEventListener("load", () => {
-     if ("pl" in params) {
-        main_img.src = params.pl;
-     }
-})
+if ("pl" in params) {
+    main_img.src = params.pl;
+}
