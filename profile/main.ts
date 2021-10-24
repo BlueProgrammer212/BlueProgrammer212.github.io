0af0e0ed5045c1e980f79c6b1c4ae5b52d805718
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-24 20:16:24 +0800

    Initial Commit

--- profile/main.ts
+++ profile/main.ts
@@ -7,3 +7,12 @@ if ("pl" in params) {
 if ("p" in params) { 
     document.getElementById("name_pfp").innerHTML = params.p;
 }
+
+let ids : string[] = ["comment_pagination", "mutual_friends_pagination", "posts_pagination"];
+
+ids.forEach(id => document.getElementById(id).addEventListener("click", () => {
+    for (let i = 0; i < document.getElementsByClassName("selected").length; ++i) {
+        document.getElementsByClassName("selected")[i].className = "";
+    }
+    document.getElementById(id).className = "selected";
+}))
