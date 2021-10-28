b87e27cfcf806e95c83ca58e952401a0497f2207
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-28 20:41:58 +0800

    Initial Commit

--- profile/main.ts
+++ profile/main.ts
@@ -2,10 +2,10 @@ let main_img : any = document.getElementById("img_pfp");
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 if ("pl" in params) {
-    main_img.src = params.pl;
+    main_img.src = `https://lh3.googleusercontent.com/a-/${params.pl}`;
 }
 if ("p" in params) { 
-    document.getElementById("name_pfp").innerHTML = `https://lh3.googleusercontent.com/a-/${params.p}`;
+    document.getElementById("name_pfp").innerHTML = params.p;
 }
 
 let ids : string[] = ["comment_pagination", "mutual_friends_pagination", "posts_pagination"];
