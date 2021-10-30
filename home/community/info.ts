273516a2e0d24190da94f59d95d5dc77b92cb529
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-10-30 20:25:41 +0800

    User interface updates

--- home/community/info.ts
+++ home/community/info.ts
@@ -341,7 +341,7 @@ class FragmentManager extends FragmentInstance implements FragmentExtension {
                 this.q.forEach(docs => {
                     console.log(`Checking ID <${docs.data().id}>`)
                     for (let o = 0; o < this.a.length; ++o) {
-                        if (docs.data().id.match(this.a[o])) {
+                        if (docs.data().id == this.a[o]) {
                             firestore.collection("posts").doc(docs.id).update({likes: data.likes+1})
                             data.likes = data.likes + 1;
                         }
