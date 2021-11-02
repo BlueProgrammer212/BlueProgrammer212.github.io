1b24badc977ce73e5701d4dc2fb5532ab1f92224
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-02 20:37:41 +0800

    Profile descriptions and Bugs hotfix in Indev 1.0.0

--- home/comments/main.js
+++ home/comments/main.js
@@ -130,18 +130,34 @@ console.log("%cPlease do not execute a malicious code here! You might give hacke
 console.log("%cSelf-XSS is a software attack to give hackers access to your account by convincing you to execute a malicious code into the developer console", "color:blue;font-size:16px");
 console.log("%cLearn more at https://blueprogrammer212.github.io/self-xss", "color:blue;font-size:16px");
 
+let firestore;
+const firebaseConfig = {
+  apiKey: "AIzaSyDqcXlXth2r-3nA-nWxUTlcm5-vgq2ZQgA",
+  authDomain: "pixcel-272e8.firebaseapp.com",
+  projectId: "pixcel-272e8",
+  storageBucket: "pixcel-272e8.appspot.com",
+  messagingSenderId: "527485563587",
+  appId: "1:527485563587:web:59c6c095e772a028802876",
+  measurementId: "G-49V48L8TZR",
+  databaseURL: "https://pixcel-272e8-default-rtdb.firebaseio.com"
+};
+
+
 window.addEventListener("load", () => {
     setTimeout(() => {
       if (auth2.isSignedIn.get()) {
         let image_url = getCookie("pfp_url"), 
         name = getCookie("pf_name"),
         id = getCookie("pf_id");
+        firebase.initializeApp(firebaseConfig);
+        firestore = firebase.firestore();
+        this.firestore.collection("profiles").doc(id).set({id: id, name: name, image_url: image_url, description: null});
         console.log(`Loading profile... ${new Profile(image_url, id, name)}`)
         console.log(`Loading username... NAME:${name}`)
         console.log(`Loading UserID... ID:<${id}>`)
         console.log(`Loading profile picture ${image_url}...`)  
         pfp_img_elem.setAttribute("src", image_url);
-        window.location.href="./page"
+        window.location.href="https://blueprogrammer212.github.io/home"
       } else {
         document.body.style = ""
       }
