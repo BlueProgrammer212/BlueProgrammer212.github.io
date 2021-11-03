f1a9b45d67d0f27ee6445522d40991d219e8d8c5
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-03 11:35:43 +0800

    Profile description and Bugs hotfix 1.0.0

--- home/comments/main.js
+++ home/comments/main.js
@@ -49,7 +49,7 @@ async function onSignIn(googleUser) {
     xhr.onload = function() {
       console.log('Signed in as: ' + xhr.responseText);
     };
-    let date_joined = await firestore.collection("profiles").doc(id).get().then(a => {if (a.exists) a.data()});
+    let date_joined = await firestore.collection("profiles").doc(id).get().then(a => a.data());
     if (!("date_joined" in date_joined)) date_joined = {date_joined: now.getFullYear()+'/'+(now.getMonth()+1)+'/'+now.getDate()};
     firestore.collection("profiles").doc(id).set({id: id, name: name, image_url: image_url, description: null, date_joined: date_joined["date_joined"]}).then(() => {
       window.location.href="https://blueprogrammer212.github.io/home/comments/page"
