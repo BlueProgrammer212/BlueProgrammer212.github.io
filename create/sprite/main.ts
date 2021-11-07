a95420b2faba61ffa89aec1573cf6a17476202c2
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-07 17:15:09 +0800

    Fixed TypeScript bugs

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -1,4 +1,4 @@
-import {Vector2} from "./js/Math";
+import {Vector2} from "./js/Math.js";
 console.log(new Vector2(0, 1))
 
 const token_id : string = '730868686856-lkanp3tois4cj938t2g794cebadtqkoo.apps.googleusercontent.com';
