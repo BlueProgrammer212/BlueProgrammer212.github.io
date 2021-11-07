ac5d4b9e99443f9001b68d9feedb73246b31d444
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-07 19:40:02 +0800

    Typescript bug fix #0002

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -1,8 +1,31 @@
-importScripts("./js/Math.ts");
 const token_id : string = '730868686856-lkanp3tois4cj938t2g794cebadtqkoo.apps.googleusercontent.com';
 
 async function loadJSON(path : string): Promise<JSON> {
     return fetch(path).then(a => a.json());
+}
+
+interface Pos2 {
+    x : number, 
+    y : number;
+}
+
+class Vector2 implements Pos2 {
+    x : number;
+    y : number;
+    constructor(x = 0, y = 0) {
+        for (let i = 0; i < arguments.length; ++i) {
+            if (arguments[i] === void 0) {
+                this.setPosition();
+                return;
+            }
+        }
+        this.setPosition(x, y);
+    }
+    setPosition(x = 0, y = 0): Pos2 {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
 }
 
 
