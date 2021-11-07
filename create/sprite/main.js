7dbaf5aca014cfc74f31e8f836adef90a75ee34b
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-07 11:00:34 +0800

    Commit: Bug hotfixes #00002

--- create/sprite/main.js
+++ create/sprite/main.js
@@ -148,6 +148,62 @@ canvas.addEventListener("touchmove", (e) => {
   drawPixel(canvas.getContext("2d"), d.x, d.y)
 })
 
+var googleUser = {};
+var startApp = function() {
+  gapi.load('auth2', function(){
+    auth2 = gapi.auth2.init({
+      client_id: '730868686856-lkanp3tois4cj938t2g794cebadtqkoo.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin'
+    });
+  });
+};
+
+startApp()
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile(),
+        id = profile.getId(), 
+        image_url = profile.getImageUrl(),
+        name = profile.getName(),
+        email = profile.getEmail();
+      
+    let pfp_elem = document.getElementsByClassName("pfp_img")[0];
+
+    pfp_elem.setAttribute("src", image_url)
+    
+    console.log('ID: ' + id);
+    console.log('Name: ' + name);
+    console.log('Image URL: ' + image_url);
+    console.log('Email: ' + email);
+
+    setCookie("pfp_url", image_url, 365)
+    setCookie("pf_id", id.toString(), 365)
+    setCookie("pf_name", name, 365)
+    setCookie("pf_email", email, 365);
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 document.getElementById("profile_href").href = `https://blueprogrammer212.github.io/profile?id=${getCookie("pf_id")}`;
 
 let pfp_img_elem = document.getElementsByClassName("pfp_img")[0];
