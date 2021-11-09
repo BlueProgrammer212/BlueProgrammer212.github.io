73286ddc84d5a03b76162645fde4bbb8d696dd6e
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-09 19:25:21 +0800

    PixiJS implementation

--- create/sprite/main.js
+++ create/sprite/main.js
@@ -97,21 +97,3 @@ window.addEventListener("load", () => {
     console.log(`Loading client content... ${document.body}`)
 })
 console.log("%cWanna be a developer?", "font-size: 15px;")
-
-var gameCanvas;
-var stage, renderer;
-var gameWidth, gameHeight;
-
-    gameCanvas = document.getElementById("main_canvas");
-
-    gameWidth = 800;
-    gameHeight = 800;
-
-var example_text = new PIXI.Text("Example text!", {font:"16px Arial", fill:"white", align:"center"});
-example_text.position.y = 100;
-example_text.position.x = 100;
-
-
-stage = new PIXI.Stage(0x000000);
-renderer = PIXI.autoDetectRenderer(gameWidth, gameHeight, gameCanvas);
-stage.addChild(example_text);
--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -1,2 +1,37 @@
+declare let PIXI: any;
 
+class Renderer {
 
+    gameCanvas: any;
+    stage: any;
+    renderer: any;
+    gameWidth: number;
+    gameHeight: number;
+
+    constructor() {
+        this.gameCanvas = null;
+        this.stage = null;
+        this.renderer = null;
+        this.gameWidth = null;
+        this.gameHeight = null;
+    }
+
+    printText(str : string, config = {font:"16px Arial", fill:"white", align:"center"}): void {
+        const example_text = new PIXI.Text(str, config);
+        this.stage.addChild(example_text)
+    }
+
+    init(cid : string, width = 800, height = 800): void {
+        this.gameCanvas = document.getElementById(cid);
+        this.stage = new PIXI.Stage(0x000000);
+        this.gameWidth = width;
+        this.gameHeight = height;
+        this.renderer = PIXI.autoDetectRenderer(this.gameWidth, this.gameHeight, this.gameCanvas);
+    }
+  
+};
+
+window.onload = function() {
+    let renderer = new Renderer();
+    renderer.init("main_canvas", 800, 800);
+}
