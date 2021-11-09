7f70391ea588e04c79b2184305dd66aaf4bc3c6d
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-09 18:21:26 +0800

    WebGL implementation

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -1,19 +1,2 @@
-declare let PIXI: any;
-
-var gameCanvas;
-var stage, renderer;
-var gameWidth, gameHeight;
-
-    gameCanvas = document.getElementById("main_canvas");
-
-    gameWidth = 800;
-    gameHeight = 800;
-
-var example_text = new PIXI.Text("Example text!", {font:"16px Arial", fill:"white", align:"center"});
-example_text.position.y = 100;
-example_text.position.x = 100;
 
 
-stage = new PIXI.Stage(0x000000);
-renderer = PIXI.autoDetectRenderer(gameWidth, gameHeight, gameCanvas);
-stage.addChild(example_text);
