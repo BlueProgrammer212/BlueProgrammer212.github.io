599aad338f5abc7bab24ae2e327df3f159a64fad
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-21 15:01:30 +0800

    Adding a notification system (Test mode)

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,35 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDqcXlXth2r-3nA-nWxUTlcm5-vgq2ZQgA",
-    authDomain: "pixcel-272e8.firebaseapp.com",
-    projectId: "pixcel-272e8",
-    storageBucket: "pixcel-272e8.appspot.com",
-    messagingSenderId: "527485563587",
-    appId: "1:527485563587:web:59c6c095e772a028802876",
-    measurementId: "G-49V48L8TZR",
-    databaseURL: "https://pixcel-272e8-default-rtdb.firebaseio.com",
+            apiKey: "Your API key.",
+            authDomain: "Your auth domain",
+            storageBucket: "Your storage bucket link",
+            projectId: "Your project ID",
+            storageBucket: "stotrageBucketID",
+            messagingSenderId: "Sender ID",
+            appId: "App ID",
+            measurementId: "measurement ID"
 });
 
+//initializing messaging service
 const messaging = firebase.messaging();
+
+//Handles background messages when tab is in background.
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        '"Firebase-messaging-sw.js" file is recieved.',payload
+    );
+
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "--You can put you .png file here.--",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+})
