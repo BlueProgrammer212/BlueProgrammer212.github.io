08ab662a34d9d7404a25e7a0ae9f3e1b85ca0970
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-21 15:03:43 +0800

    Adding a notification system (Test mode)

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,16 +2,18 @@
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
-firebase.initializeApp({
-            apiKey: "Your API key.",
-            authDomain: "Your auth domain",
-            storageBucket: "Your storage bucket link",
-            projectId: "Your project ID",
-            storageBucket: "stotrageBucketID",
-            messagingSenderId: "Sender ID",
-            appId: "App ID",
-            measurementId: "measurement ID"
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyDqcXlXth2r-3nA-nWxUTlcm5-vgq2ZQgA",
+    authDomain: "pixcel-272e8.firebaseapp.com",
+    projectId: "pixcel-272e8",
+    storageBucket: "pixcel-272e8.appspot.com",
+    messagingSenderId: "527485563587",
+    appId: "1:527485563587:web:59c6c095e772a028802876",
+    measurementId: "G-49V48L8TZR",
+    databaseURL: "https://pixcel-272e8-default-rtdb.firebaseio.com",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 //initializing messaging service
 const messaging = firebase.messaging();
@@ -22,10 +24,10 @@ messaging.setBackgroundMessageHandler(function(payload) {
         '"Firebase-messaging-sw.js" file is recieved.',payload
     );
 
-    const notificationTitle = "Background Message Title";
+    const notificationTitle = "Pixcel";
     const notificationOptions = {
-        body: "Background Message body.",
-        icon: "--You can put you .png file here.--",
+        body: "Hello there! I am hoping that you can see this little notification test. Please tell BlueProgrammer on discord that this notification test worked.",
+        icon: "https://firebasestorage.googleapis.com/v0/b/pixcel-272e8.appspot.com/o/logo_icon.png?alt=media&token=2e685f22-a5a9-4a1a-b4c5-a2cd91641b09",
     };
 
     return self.registration.showNotification(
