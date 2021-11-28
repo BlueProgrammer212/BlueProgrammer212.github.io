5a8432827ad790d4cbd1bb8f6e84baa02fe3fc29
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-28 10:39:47 +0800

    Improving UI experience in the Sprite editor

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -14,7 +14,6 @@ var gkhead = new Image;
 window.onload = function(){		
 
 var ctx : CanvasRenderingContext2D = canvas.getContext('2d');
-trackTransforms(ctx);
 
 let pixels = [];
         
@@ -115,83 +114,5 @@ function redraw(){
         onMouseDownPencilMode = false;
     },false);
 
-    var scaleFactor = 1.6;
-
-    var zoom = function(clicks){
-        pt = ctx.transformedPoint(lastX,lastY);
-        ctx.translate(pt.x,pt.y);
-        factor = Math.pow(scaleFactor,clicks);
-        ctx.scale(factor,factor);
-        ctx.translate(-pt.x,-pt.y);
-        redraw();
-    }
-
-    var handleScroll = function(evt){
-        var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
-        if (delta) zoom(delta);
-        return evt.preventDefault() && false;
-    };
-
-    canvas.addEventListener('DOMMouseScroll',handleScroll,false);
-    canvas.addEventListener('mousewheel',handleScroll,false);
 };
 
-function trackTransforms(ctx){
-    var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
-    var xform = svg.createSVGMatrix();
-    ctx.getTransform = function(){ return xform; };
-
-    var savedTransforms = [];
-    var save = ctx.save;
-    ctx.save = function(){
-        savedTransforms.push(xform.translate(0,0));
-        return save.call(ctx);
-    };
-
-    var restore = ctx.restore;
-    ctx.restore = function(){
-    xform = savedTransforms.pop();
-    return restore.call(ctx);
-            };
-
-    var scale = ctx.scale;
-    ctx.scale = function(sx,sy){
-    xform = xform.scaleNonUniform(sx,sy);
-    return scale.call(ctx,sx,sy);
-            };
-
-    var rotate = ctx.rotate;
-    ctx.rotate = function(radians){
-        xform = xform.rotate(radians*180/Math.PI);
-        return rotate.call(ctx,radians);
-    };
-
-    var translate = ctx.translate;
-    ctx.translate = function(dx,dy){
-        xform = xform.translate(dx,dy);
-        return translate.call(ctx,dx,dy);
-    };
-
-    var transform = ctx.transform;
-    ctx.transform = function(a,b,c,d,e,f){
-        var m2 = svg.createSVGMatrix();
-        m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
-        xform = xform.multiply(m2);
-        return transform.call(ctx,a,b,c,d,e,f);
-    };
-
-    var setTransform = ctx.setTransform;
-    ctx.setTransform = function(a,b,c,d,e,f){
-        const xform_prop : string[] = ["a","b","c","d","e","f"];
-        for (let xformi=0;xformi<xform_prop.length;++xformi) {
-            if (arguments[xformi]!==void 0) xform[xform_prop[xformi]]==arguments[xformi];
-        }
-        return setTransform.call(ctx,a,b,c,d,e,f);
-    };
-
-    var pt  = svg.createSVGPoint();
-    ctx.transformedPoint = function(x,y){
-        pt.x=x; pt.y=y;
-        return pt.matrixTransform(xform.inverse());
-    }
-}
