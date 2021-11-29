ffba16f95ee55682fbcad36366141b13226384b6
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-29 16:04:49 +0800

    Fixing some security vulnerabilities

--- home/community/community.js
+++ home/community/community.js
@@ -61,7 +61,9 @@ let image_url_;
         
         let pfp_elem = document.getElementsByClassName("pfp_img")[0];
 
-        pfp_elem.setAttribute("src", image_url)
+        firestore.collection("profiles").doc(getCookie("pf_id")).get().then(pfp_info => {
+          pfp_elem.setAttribute("src", pfp_info.data().image_url);
+        }).catch(e => console.error(`Something unexpected occured. ${e}`));
 
         console.log('ID: ' + id);
         console.log('Name: ' + name);
