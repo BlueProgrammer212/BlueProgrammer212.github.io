419d21aa3837c3645f15aecdf69f09e96785978e
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-29 16:16:37 +0800

    Fixing some security vulnerabilities

--- home/community/main.js
+++ home/community/main.js
@@ -160,13 +160,13 @@ let image_url_ = getCookie("pfp_url") || "../assets/default_pfp_16x16.png";
           console.log(`Loading UserID... Id:<${id}>`);
           console.log(`Loading profile picture ${image_url}...`);
   
-          firestore.collection("profiles").doc(getCookie("pf_id")).get().then(pfp_info => {
-            pfp_img_elem.setAttribute("src", pfp_info.data().image_url);
-          }).catch(e => console.error(`Something unexpected occured. ${e}`));
           firebase.initializeApp(firebaseConfig);
           firestore = firebase.firestore();
           let fileUpload = document.getElementById("uploadImage")
-        
+          firestore.collection("profiles").doc(getCookie("pf_id")).get().then(pfp_info => {
+            pfp_img_elem.setAttribute("src", pfp_info.data().image_url);
+          }).catch(e => console.error(`Something unexpected occured. ${e}`));
+          
           fileUpload.addEventListener('change', function(evt) {
               let generatedFileName = generateName();
               let storageRef = firebase.storage().ref(`uploads/${generatedFileName}.png`)
