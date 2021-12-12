2149a7fa44cb516f339d02848c07afe9c20c1cd6
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-12 18:31:52 +0800

    Making the sprite system #0023

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -188,6 +188,7 @@ function animate() {
     let canvas_preview : any = document.getElementById("preview_canvas_sprite_animation");
     if (document.getElementsByClassName("spriteBoxContainer")[cf + 1]) {cf += 1;} else {cf = 0;}
     const buffer = document.getElementsByClassName("spriteBoxContainer")[cf].children[1];
+    if (cf !== 0) canvas_preview.getContext("2d").clearRect(0, 0, canvas_preview.width, canvas_preview.height)
     canvas_preview.getContext("2d").imageSmoothingEnabled = false;
     canvas_preview.getContext("2d").drawImage(buffer, 0, 0, canvas_preview.width, canvas_preview.height);
 }
