d933eafc41594f62a2600d199b03db455990fdeb
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-18 11:04:21 +0800

    Calculating pixel propagation

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -227,11 +227,12 @@ const adjacent : Vec2[] = [
 
 function fill(x : number, y : number) : void {
     let p = [{x: x * psize, y: y * psize}]
-    for (let r = 0; r < p.length; ++r) {
+    for (let r = 0; r < p.length; r++) {
         for (let {x, y} of adjacent) {
             let dx = p[r].x + (x * psize), dy = p[r].y + (y * psize);
             if (dx >= 0 && dx <= canvas.width && 
-                dy >= 0 && dy <= canvas.height) {
+                dy >= 0 && dy <= canvas.height &&
+                !p.some(pa => pa.x == dx && pa.y == dy)) {
                 p.push({x: dx, y: dy}); 
                 drawPixel(context, dx, dy, psize);    
             }   
