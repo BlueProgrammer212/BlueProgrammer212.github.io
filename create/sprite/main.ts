8c5330411b07ea1fb5453524ee7e73b3c299214e
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-20 21:51:20 +0800

    Floodfill algorithm implementation

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -480,7 +480,7 @@ class CanvasManager implements CanvasInterface {
 
     public static fill({ x, y }: { x: number; y: number; }): void {
         console.log(`Starting position: ${new Vec2(x, y)}`);
-        drawPixel(context, x * 16, y * 16, 16)
+        drawPixel(context, x, y, 16)
     }
     
 }
@@ -701,7 +701,7 @@ canvas.addEventListener("mousedown", (e) => {
         }
         if (currentTool == "Bucket") {
             let modifiedVector = getMousePos(canvas, e.x, e.y);
-            CanvasManager.fill({ x: modifiedVector.x, y: modifiedVector.y });
+            CanvasManager.fill({ x: e.x, y: e.y });
             updateFrame()
         }
     } else if (e.button == 2) {
