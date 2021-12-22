bc65fb5b82d2e25bbcf9fbc31cbe82d4559eccdc
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-22 10:42:17 +0800

    Removed layer functionality (again)

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -510,11 +510,8 @@ function updateFrame() {
         let sprite_canvas = document.getElementsByClassName("spriteBoxContainer")[selected_sprite_frame_index].children[1];
         sprite_canvas["getContext"]("2d").imageSmoothingEnabled = false;
         sprite_canvas["getContext"]("2d").clearRect(0, 0, sprite_canvas["width"], sprite_canvas["height"]) 
-        let layers = document.getElementsByClassName("LayerBoxCanvasPreview");
-        for (let j = 0; j < layers.length; ++j) {
-            sprite_canvas["getContext"]("2d").drawImage(layers[j],
-            0, 0, sprite_canvas["width"], sprite_canvas["height"]) 
-        }
+        sprite_canvas["getContext"]("2d").drawImage(document.getElementById("main_canvas"),
+        0, 0, sprite_canvas["width"], sprite_canvas["height"]) 
     }
 }
 
@@ -524,7 +521,7 @@ function onSpriteSwitch() {
     .filter(a => { return a.className.includes("selected")});
     old_frame.forEach(elem => elem.className = "spriteBoxContainer");
     document.getElementsByClassName("spriteBoxContainer")[selected_sprite_frame_index].className += " selected";
-    let sprite_canvas : any = document.getElementsByClassName("LayerBoxCanvasPreview")[currentLayerSelected];
+    let sprite_canvas : any = document.getElementsByClassName("spriteBoxContainer")[selected_sprite_frame_index].children[1];
     context.imageSmoothingEnabled = false;
     context.drawImage(sprite_canvas, 0, 0, canvas.width, canvas.height);
 }
