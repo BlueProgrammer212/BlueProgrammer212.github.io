2019dfbdc3f7e8d4568f7b7a29c3219133c99104
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-26 21:18:17 +0800

    Updated release_app.bat

--- release_app.bat
+++ release_app.bat
@@ -2,6 +2,11 @@
 SETLOCAL 
 set /p release_name=App title: 
 set /p description=Description:
+set /p version=Version:
+set /p allowLogs=Enable publish logs (Y/N): 
+IF not exist publish_logs mkdir publish_logs
+IF %allowLogs%==Y cd publish_logs && @echo Release date: %DATE% Release Name: %release_name% > publish_log_record.log
 echo Release date: %DATE%
+echo Publishing %release_name%, version %version%
 ENDLOCAL
 
