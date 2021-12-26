431570a5badd035c22c7f03c6fa6b0efa99256e5
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-26 21:06:55 +0800

    Created release_app.bat to simplify CLI interface

--- publish.bat
+++ publish.bat
@@ -1,6 +1,7 @@
 @echo off
+SETLOCAL
 set /p msg=Enter commit message: 
-echo %msg%
 git add --all
 git commit -m "%msg%"
 git push -u origin main
+ENDLOCAL
---
+++ release_app.bat
@@ -0,0 +1,7 @@
+@echo off
+SETLOCAL 
+set /p release_name=App title: 
+set /p description=Description:
+echo Release date: %DATE%
+ENDLOCAL
+
