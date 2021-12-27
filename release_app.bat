60a421a5e4fdf981a982ccef1a8a71cc12aed98a
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-27 13:44:24 +0800

    Added new release, 27/12/2021 Test Test

--- release_app.bat
+++ release_app.bat
@@ -8,10 +8,12 @@ IF not exist publish_logs mkdir publish_logs
 IF %allowLogs%==Y cd publish_logs && @echo Release date: %DATE% Release Name: %release_name% > publish_log_record.log
 echo Release date: %DATE%
 echo Publishing %release_name%, version %version%; sleep 5;
-echo Published %release_name% %version% successfully.
 git add --all
 git commit -m "Added new release, %DATE% %release_name% %version%"
 set /p pullNewCommits=Pull new commits? (Y/N):
 IF %pullNewCommits%==Y git pull -v
 git push -u origin main
+node ../publish_release.js
+sleep 5;
+echo Published %release_name% %version% successfully! Progress: [▯▯▯▯▯▯▯▯▯▯▯▯▯▯] 100%.
 ENDLOCAL
