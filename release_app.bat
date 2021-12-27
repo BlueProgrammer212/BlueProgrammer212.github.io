5c0d5e8d56c1878c58d015454eaad1ac9c541c5c
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-27 13:50:20 +0800

    Added new release, 27/12/2021 Pixcel lol

--- release_app.bat
+++ release_app.bat
@@ -8,18 +8,18 @@ IF not exist publish_logs mkdir publish_logs
 IF %allowLogs%==Y cd publish_logs && @echo Release date: %DATE% Release Name: %release_name% > publish_log_record.log
 echo Release date: %DATE%
 echo Publishing %release_name%, version %version%; 
-sleep 1;
+timeout 1;
 git add --all
 git commit -m "Added new release, %DATE% %release_name% %version%"
 set /p pullNewCommits=Pull new commits? (Y/N):
 IF %pullNewCommits%==Y git pull -v
 echo Progress: [---------------------------------]: 0%
 git push -u origin main
-sleep 3;
+timeout 3;
 echo Progress: [################-----------------]: 23%
 node ../publish_release.js
-sleep 3;
+timeout 3;
 echo Progress: [#######################----------]: 71%
-sleep 3;
+timeout 3;
 echo Published %release_name% %version% successfully! Progress: [######################################] 100%.
 ENDLOCAL
