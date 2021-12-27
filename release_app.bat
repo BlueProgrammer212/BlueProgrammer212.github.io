0f4bd02ac00d16168104e7e264f009f946fc3e8b
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-27 13:54:36 +0800

    Added new release, 27/12/2021 Pixcel Test

--- release_app.bat
+++ release_app.bat
@@ -8,18 +8,18 @@ IF not exist publish_logs mkdir publish_logs
 IF %allowLogs%==Y cd publish_logs && @echo Release date: %DATE% Release Name: %release_name% > publish_log_record.log
 echo Release date: %DATE%
 echo Publishing %release_name%, version %version%; 
-timeout 1  NUL;
+ping www.pixcel.ml -n 3 > nul
 git add --all
 git commit -m "Added new release, %DATE% %release_name% %version%"
 set /p pullNewCommits=Pull new commits? (Y/N):
 IF %pullNewCommits%==Y git pull -v
 echo Progress: [---------------------------------]: 0%
 git push -u origin main
-timeout 3 > NUL;
+ping www.pixcel.ml -n 3 > nul
 echo Progress: [################-----------------]: 23%
 node ../publish_release.js
-timeout 3 > NUL;
+ping www.pixcel.ml -n 3 > nul
 echo Progress: [#######################----------]: 71%
-timeout 3 > NUL;
+ping www.pixcel.ml -n 3 > nul
 echo Published %release_name% %version% successfully! Progress: [######################################] 100%.
 ENDLOCAL
