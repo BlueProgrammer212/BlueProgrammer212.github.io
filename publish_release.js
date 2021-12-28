935a967aea00c3fd74a0fdcc2097a97810cb0c39
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-28 16:13:35 +0800

    Added new release, 28/12/2021 Pixcel 1.0.0

--- publish_release.js
+++ publish_release.js
@@ -8,9 +8,15 @@ initializeApp({
 });
 
 const db = getFirestore();
+
 const docRef = db.collection('download_releases');
+let description_string = "";
+for (let i = 1; i < arguments.length - 1; ++i) {
+   description_string += " " + arguments[i];
+}
+
 docRef.add({
-  description: arguments[1],
+  description: description_string,
   name: arguments[0],
   version: arguments[2]
 })
