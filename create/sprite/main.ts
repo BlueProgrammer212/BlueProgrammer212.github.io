3e8f440dee53fe64bfd53e32621222f73fd77ab5
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-12-31 15:54:58 +0800

    Zooming functionality

--- create/sprite/main.ts
+++ create/sprite/main.ts
@@ -1185,7 +1185,7 @@ document.getElementById("clearCanvasButton").addEventListener("click", () => {
 })
 let old_scale = 1;
 canvas_overlay_context.canvas.addEventListener("wheel", (ev) => {
-    let d = Math.sign(ev.deltaY) * 0.25;
+    let d = -Math.sign(ev.deltaY) * 0.25;
     canvases.forEach(e => e.style.transform = `scale(${d + old_scale}) translate(-${50 * (d + old_scale)}%, -${46 * (d + old_scale)}%)`)
     old_scale = old_scale + d;
 });
